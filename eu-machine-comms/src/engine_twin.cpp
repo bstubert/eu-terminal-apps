b843f7c492383617d2b36addbf10470ebbd69844
Burkhard Stubert <burkhard.stubert@embeddeduse.com>
2021-10-09 09:00:55 +0200

    Created library eu-machine-comms for communication with machine over CAN bus. Added skeleton class for EngineTwin.

---
+++ eu-machine-comms/src/engine_twin.cpp
@@ -0,0 +1,9 @@
+// Copyright, Burkhard Stubert (burkhard.stubert@embeddeduse.com)
+
+#include "engine_twin.h"
+
+EngineTwin::EngineTwin(QObject *parent)
+    : QObject(parent)
+{
+
+}
---
+++ eu-machine-comms/src/engine_twin.h
@@ -0,0 +1,15 @@
+// Copyright, Burkhard Stubert (burkhard.stubert@embeddeduse.com)
+
+#pragma once
+
+#include <QObject>
+
+class EngineTwin : public QObject
+{
+    Q_OBJECT
+public:
+    explicit EngineTwin(QObject *parent = nullptr);
+
+signals:
+
+};
