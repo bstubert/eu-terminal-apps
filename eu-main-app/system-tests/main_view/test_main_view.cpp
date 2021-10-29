0632fb2318b48e2ba656bb0b24d8183cf50aeead
Burkhard Stubert <burkhard.stubert@embeddeduse.com>
2021-10-29 18:19:03 +0200

    Introduced skeleton for system test TestMainView.

---
+++ eu-main-app/system-tests/main_view/test_main_view.cpp
@@ -0,0 +1,32 @@
+// Copyright, Burkhard Stubert (burkhard.stubert@embeddeduse.com)
+
+#include <QtTest>
+
+class TestMainView : public QObject
+{
+    Q_OBJECT
+
+private slots:
+    void init();
+    void cleanup();
+    void testEngineSpeed();
+
+private:
+};
+
+void TestMainView::init()
+{
+}
+
+void TestMainView::cleanup()
+{
+}
+
+void TestMainView::testEngineSpeed()
+{
+    QVERIFY(false);
+}
+
+QTEST_GUILESS_MAIN(TestMainView)
+
+#include "test_main_view.moc"
