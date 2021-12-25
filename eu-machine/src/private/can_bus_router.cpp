d6b02cd8209366f2d87f4527c9e78a3713dd15fb
Burkhard Stubert <burkhard.stubert@embeddeduse.com>
2021-12-25 19:08:46 +0100

    Moved decoding of CAN frame payload to Quantity (temporarily).

--- eu-machine/src/private/can_bus_router.cpp
+++ eu-machine/src/private/can_bus_router.cpp
@@ -4,14 +4,6 @@
 
 #include "can_bus_router.h"
 
-namespace
-{
-    bool isFrameFromEngine(int frameId)
-    {
-        return (frameId & 0xff) == 0x00;
-    }
-}
-
 CanBusRouter::CanBusRouter(QCanBusDevice *canBus, QObject *parent)
     : QObject{parent}
     , m_canBus{canBus}
@@ -31,28 +23,10 @@ void CanBusRouter::onFramesReceived()
     QList<Quantity> quantityColl;
     for (const auto &frame : m_canBus->readAllFrames())
     {
-        if (isFrameFromEngine(frame.frameId()))
+        Quantity q{frame.frameId(), frame.payload()};
+        if (q.sourceAddress() == Quantity::SourceAddress::Engine)
         {
-            switch (frame.frameId())
-            {
-            case 0xCF00400:
-            {
-                auto payload = frame.payload();
-                auto rpm = qFromLittleEndian<quint16>(payload.data() + 3) / 8.0;
-                quantityColl.append(Quantity{Quantity::Pgn::EEC1, rpm, u"rpm"_qs});
-                break;
-            }
-            case 0x18FEF100:
-            {
-                auto payload = frame.payload();
-                auto kph = qFromLittleEndian<quint16>(payload.data() + 1) / 256.0;
-                quantityColl.append(Quantity{Quantity::Pgn::CCVS1, kph, u"kph"_qs});
-                break;
-            }
-            default:
-                break;
-            }
-
+            quantityColl.append(q);
         }
     }
     emit newEngineQuantities(quantityColl);
--- eu-machine/src/quantity.cpp
+++ eu-machine/src/quantity.cpp
@@ -1,8 +1,47 @@
 // Copyright, Burkhard Stubert (burkhard.stubert@embeddeduse.com)
 
+#include <QtEndian>
+
 #include "quantity.h"
 
+namespace
+{
+qreal toRealValue(Quantity::Pgn pgn, const QByteArray &payload)
+{
+    switch (pgn)
+    {
+    case Quantity::Pgn::EEC1:
+        return qFromLittleEndian<quint16>(payload.data() + 3) / 8.0;
+    case Quantity::Pgn::CCVS1:
+        return qFromLittleEndian<quint16>(payload.data() + 1) / 256.0;
+    default:
+        return 0.0;
+    }
+}
+
+QString toUnit(Quantity::Pgn pgn)
+{
+    switch (pgn)
+    {
+    case Quantity::Pgn::EEC1:
+        return u"rpm"_qs;
+    case Quantity::Pgn::CCVS1:
+        return u"kph"_qs;
+    default:
+        return {};
+    }
+}
+}
+
 Quantity::Quantity()
+{
+}
+
+Quantity::Quantity(quint32 frameId, const QByteArray &payload)
+    : m_pgn{static_cast<Quantity::Pgn>(frameId >> 8)}
+    , m_sourceAddress{static_cast<Quantity::SourceAddress>(frameId & 0xff)}
+    , m_value{toRealValue(m_pgn, payload)}
+    , m_unit{toUnit(m_pgn)}
 {
 }
 
@@ -16,6 +55,11 @@ Quantity::Quantity(Pgn pgn, qreal value, const QString &unit)
 Quantity::Pgn Quantity::pgn() const
 {
     return m_pgn;
+}
+
+Quantity::SourceAddress Quantity::sourceAddress() const
+{
+    return m_sourceAddress;
 }
 
 qreal Quantity::value() const
